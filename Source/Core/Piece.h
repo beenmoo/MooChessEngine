e3f7a8a3237b59a3461670738e61a602f599a880
benjaminmao123 <43651347+benjaminmao123@users.noreply.github.com>
2026-08-16 23:28:04 +0900

    presets and piece

--- Source/Core/Piece.h
+++ Source/Core/Piece.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 namespace moo
 {
 
@@ -8,5 +10,23 @@ namespace moo
 // used by Board, Move, and Evaluation.
 //
 // TODO: represent piece types and colors (e.g. enums, bit flags).
+enum class Piece : uint8_t
+{
+    None = 0,
+
+    WhitePawn = 1,    // 0b0001
+    WhiteKnight = 2,  // 0b0010
+    WhiteBishop = 3,  // 0b0011
+    WhiteRook = 4,    // 0b0100
+    WhiteQueen = 5,   // 0b0101
+    WhiteKing = 6,    // 0b0110
+
+    BlackPawn = 9,     // 0b1001
+    BlackKnight = 10,  // 0b1010
+    BlackBishop = 11,  // 0b1011
+    BlackRook = 12,    // 0b1100
+    BlackQueen = 13,   // 0b1101
+    BlackKing = 14,    // 0b1110
+};
 
 }  // namespace moo
--- Source/Uci/UciEngine.cpp
+++ Source/Uci/UciEngine.cpp
@@ -16,6 +16,24 @@ void UciEngine::Run()
     while (std::getline(std::cin, line))
     {
         // TODO: dispatch UCI commands here.
+        if (line == "uci")
+        {
+            std::cout << "id name MooChessEngine" << std::endl;
+            std::cout << "id author Your Name" << std::endl;
+            std::cout << "uciok" << std::endl;
+        }
+        else if (line == "isready")
+        {
+            std::cout << "readyok" << std::endl;
+        }
+        else if (line == "ucinewgame")
+        {
+            // TODO: reset engine state for a new game
+        }
+        else if (line == "quit")
+        {
+            break;
+        }
     }
 }
 
